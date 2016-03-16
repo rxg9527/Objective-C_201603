a235a5016eade880758940b3c3ac8c150b870b70
Yuen-rMBP <rxg9527@sina.cn>
2016-03-16 16:10:22 +0800

    初始化项目

---
+++ NSDate_Week&Lunar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSDate_Week&Lunar
+//
+//  Created by Yuen on 16/3/16.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSDate_Week&Lunar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSDate_Week&Lunar
+//
+//  Created by Yuen on 16/3/16.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
