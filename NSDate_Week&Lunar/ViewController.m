d8c7330cc658c3a35ba0c3f75f4bb03f64dcdc70
Yuen-rMBP <rxg9527@sina.cn>
2016-03-16 16:24:06 +0800

    阮晓刚 20160316 判断NSDate是星期几

--- NSDate_Week&Lunar/ViewController.m
+++ NSDate_Week&Lunar/ViewController.m
@@ -17,11 +17,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"%@", [self weekdayStringFromDate:[NSDate date]]);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/**
+ *  判断NSDate是星期几
+ *
+ *  @param inputDate
+ *
+ *  @return 字符串
+ */
+- (NSString*)weekdayStringFromDate:(NSDate*)inputDate {
+    
+    NSArray *weekdays = [NSArray arrayWithObjects: [NSNull null], @"星期天", @"星期一", @"星期二", @"星期三", @"星期四", @"星期五", @"星期六", nil];
+    
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    
+    NSTimeZone *timeZone = [[NSTimeZone alloc] initWithName:@"Asia/Shanghai"];
+    
+    [calendar setTimeZone: timeZone];
+    
+    NSCalendarUnit calendarUnit = NSCalendarUnitWeekday;
+    
+    NSDateComponents *theComponents = [calendar components:calendarUnit fromDate:inputDate];
+    
+    return [weekdays objectAtIndex:theComponents.weekday];
+    
 }
 
 @end
