82e0a667726af9d0cab81e8f5426a444bf6642b0
Yuen-rMBP <rxg9527@sina.cn>
2016-03-16 16:03:21 +0800

    Initial Commit

---
+++ Demo201603/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo201603
+//
+//  Created by Yuen on 16/3/16.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo201603/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo201603
+//
+//  Created by Yuen on 16/3/16.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
