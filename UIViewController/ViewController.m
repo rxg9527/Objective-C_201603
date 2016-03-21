078ff5f614351415d352ba9cdc5c8eb40f756c4c
Yuen-iMac <rxg9527@sina.cn>
2016-03-21 16:23:10 +0800

    0321 LoadView的生命周期

---
+++ UIViewController/ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ViewController.m
+//  UIViewController
+//
+//  Created by Yuen-iMac on 16/3/21.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+/* (1) View Management：管理View
+ * (2) Data Marshalling：管理数据
+ * (3) User Interactions：响应用户交互
+ * (4) Resource Management：管理资源
+ * (5) Adaptivity：适配不同的屏幕尺寸空间的变化
+ * */
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+/*定制VC时，如果覆盖loadView方法，不需要调用[super loadView]方法*/
+- (void)loadView {
+    NSLog(@"%s", sel_getName(_cmd));
+
+    UIView *view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [view setBackgroundColor:[UIColor redColor]];
+    self.view = view;
+}
+
+- (void)viewDidLoad {
+    NSLog(@"%s", __func__);
+
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
