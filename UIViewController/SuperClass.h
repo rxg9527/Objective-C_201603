3cb4638b7d34db58ff6dc38922467e8c7b60a866
Yuen-iMac <rxg9527@sina.cn>
2016-03-21 17:36:38 +0800

    0321 总结+(void)load和+(void)initialize

--- UIViewController/SuperClass.h
+++ UIViewController/SuperClass.h
@@ -5,7 +5,13 @@
 
 #import <Foundation/Foundation.h>
 
-
+/*总结：
+ *
+ *                               +(void)load	    +(void)initialize
+ * 执行时机	                 在程序运行后立即执行	  在类的方法第一次被调时执行
+ * 若自身未定义，是否沿用父类的方法？	否	                    是
+ * 类别中的定义	          全都执行，但后于类中的方法  覆盖类中的方法，只执行一个
+ * */
 @interface SuperClass : NSObject
 @end
 
