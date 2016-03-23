ff3a87b0bb913b005be7195b31ca396f6b2c5180
Yuen-iMac <rxg9527@sina.cn>
2016-03-23 13:49:42 +0800

    0323 使用animateWithDuration简单地控制页面切换效果

--- UIView/ViewController.m
+++ UIView/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *addressEmptyImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *collectionEmptyImageView;
+
 @end
 
 @implementation ViewController
@@ -17,11 +20,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.addressEmptyImageView.alpha = 0.0;
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)animationShow:(NSInteger)index {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:1.0f];
+    
+    if (index == 0) {
+        [UIView animateWithDuration:1 animations:^{
+            self.addressEmptyImageView.alpha = 1.0;
+            self.collectionEmptyImageView.alpha = 0.0;
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:4.0
+                             animations:^{
+                                 self.addressEmptyImageView.center = CGPointMake(500.0, 470.0);
+                                 self.collectionEmptyImageView.center = CGPointMake(160.0, 230.0);
+                             }];
+        }];
+    } else {
+        [UIView animateWithDuration:1 animations:^{
+            self.collectionEmptyImageView.alpha = 1.0;
+            self.addressEmptyImageView.alpha = 0.0;
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:4.0
+                             animations:^{
+                                 self.collectionEmptyImageView.center = CGPointMake(500.0, 470.0);
+                                 self.addressEmptyImageView.center = CGPointMake(160.0, 230.0);
+                             }];
+        }];
+    }
+}
+
+- (IBAction)changePage:(id)sender {
+    static BOOL bAgain = TRUE;
+    if (bAgain) {
+        [self.collectionEmptyImageView removeFromSuperview];
+        [self.view addSubview:self.addressEmptyImageView];
+        [self animationShow:0];
+    } else {
+        [self.addressEmptyImageView removeFromSuperview];
+        [self.view addSubview:self.collectionEmptyImageView];
+        [self animationShow:1];
+    }
+    bAgain = !bAgain;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 @end
