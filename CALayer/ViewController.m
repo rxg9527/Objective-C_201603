3c457a2731097492aab998a9d080b765390ebff1
Yuen-rMBP <rxg9527@sina.cn>
2016-03-23 23:49:36 +0800

    0323 CALayer自定义绘画内容到图层

--- CALayer/ViewController.m
+++ CALayer/ViewController.m
@@ -21,6 +21,7 @@
     self.view.layer.backgroundColor = [UIColor orangeColor].CGColor;
     self.view.layer.cornerRadius = 20.0;
     
+    
     /**
      *  子层
      */
@@ -47,6 +48,68 @@
     imageLayer.masksToBounds = YES;
     [sublayer addSublayer:imageLayer];  
 
+    /**
+     *  自定义绘画内容到图层
+     */
+    CALayer *customDrawn = [CALayer layer];
+    customDrawn.delegate = self;
+    customDrawn.backgroundColor = [UIColor greenColor].CGColor;
+    customDrawn.frame = CGRectMake(30, 250, 280, 200);
+    customDrawn.shadowOffset = CGSizeMake(0, 3);
+    customDrawn.shadowRadius = 5.0;
+    customDrawn.shadowColor = [UIColor blackColor].CGColor;
+    customDrawn.shadowOpacity = 0.8;
+    customDrawn.cornerRadius = 10.0;
+    customDrawn.borderColor = [UIColor blackColor].CGColor;
+    customDrawn.borderWidth = 2.0;
+    customDrawn.masksToBounds = YES;
+    [self.view.layer addSublayer:customDrawn];
+}
+
+static inline double radians (double degrees) { return degrees * M_PI/180; }
+
+void MyDrawColoredPattern (void *info, CGContextRef context) {
+    
+    CGColorRef dotColor = [UIColor colorWithHue:0 saturation:0 brightness:0.07 alpha:1.0].CGColor;
+    CGColorRef shadowColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.1].CGColor;
+    
+    CGContextSetFillColorWithColor(context, dotColor);
+    CGContextSetShadowWithColor(context, CGSizeMake(0, 1), 1, shadowColor);
+    
+    CGContextAddArc(context, 3, 3, 4, 0, radians(360), 0);
+    CGContextFillPath(context);
+    
+    CGContextAddArc(context, 16, 16, 4, 0, radians(360), 0);
+    CGContextFillPath(context);
+    
+}
+
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)context {
+    
+    CGColorRef bgColor = [UIColor colorWithHue:0 saturation:0 brightness:0.15 alpha:1.0].CGColor;
+    CGContextSetFillColorWithColor(context, bgColor);
+    CGContextFillRect(context, layer.bounds);
+    
+    static const CGPatternCallbacks callbacks = { 0, &MyDrawColoredPattern, NULL };
+    
+    CGContextSaveGState(context);
+    CGColorSpaceRef patternSpace = CGColorSpaceCreatePattern(NULL);
+    CGContextSetFillColorSpace(context, patternSpace);
+    CGColorSpaceRelease(patternSpace);
+    
+    CGPatternRef pattern = CGPatternCreate(NULL,
+                                           layer.bounds,
+                                           CGAffineTransformIdentity,
+                                           24,
+                                           24,
+                                           kCGPatternTilingConstantSpacing,
+                                           true,
+                                           &callbacks);
+    CGFloat alpha = 1.0;
+    CGContextSetFillPattern(context, pattern, &alpha);
+    CGPatternRelease(pattern);
+    CGContextFillRect(context, layer.bounds);
+    CGContextRestoreGState(context);
 }
 
 - (void)didReceiveMemoryWarning {
