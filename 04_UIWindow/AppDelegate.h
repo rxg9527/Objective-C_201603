980d38e92a312d9e3f264dc7a83dde67a21ee175
Yuen-iMac <rxg9527@sina.cn>
2016-03-24 13:21:34 +0800

    0324 初始化

---
+++ 04_UIWindow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  04_UIWindow
+//
+//  Created by Yuen-iMac on 16/3/24.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 04_UIWindow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  04_UIWindow
+//
+//  Created by Yuen-iMac on 16/3/24.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
