24c1f924686b892f5ea83209dcf7dbb76d739935
Yuen-rMBP <rxg9527@sina.cn>
2016-03-24 22:31:43 +0800

    UIScreen和UIEdgeInsets

---
+++ 13_UIScreen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  13_UIScreen
+//
+//  Created by Yuen on 16/3/24.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
