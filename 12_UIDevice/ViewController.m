0f443063eccfff6f18da15fb0a64c8bb18ebef99
Yuen-rMBP <rxg9527@sina.cn>
2016-03-24 22:18:49 +0800

    UIDevice区分设备状态

--- 12_UIDevice/ViewController.m
+++ 12_UIDevice/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"设备名——%@，\n系统版本号——%@，\n屏幕旋转方向——%@，\n区分iPad还是iPhone——%@", [UIDevice currentDevice].name, [UIDevice currentDevice].systemVersion, @([UIDevice currentDevice].orientation), @([UIDevice currentDevice].userInterfaceIdiom));
 }
 
 - (void)didReceiveMemoryWarning {
