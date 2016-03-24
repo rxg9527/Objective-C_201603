f0bb97d22ee7580ca9c57fb3c61df672d87e647e
Yuen-rMBP <rxg9527@sina.cn>
2016-03-24 21:29:42 +0800

    0324 加载图片几种方式 [UIImage imageNamed:@“xxx”] 系统缓存到cache中 [UIImage imageWithContentsOfFile:path] 不缓存 [UIImage imageWithData:data]  不缓存

--- 05_UIImageView/ViewController.m
+++ 05_UIImageView/ViewController.m
@@ -17,11 +17,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
+    
+    //1.第一种方法
+    //从一个文件读取一张图片
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"map" ofType:@"png"];
+    //每次都从文件中读取,适用于我们在程序中只使用一次的图片
+    //不会存储缓存,适用于比较大的图片
+    UIImage *image1 = [UIImage imageWithContentsOfFile:path];
+    NSLog(@"%f,%f", image1.size.width, image1.size.height);
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image1];
+    imageView.frame = CGRectMake(0, 100, image1.size.width, image1.size.height);
+    [self.view addSubview:imageView];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    //2.第二种方式
+    //也不会存储缓存
+    //从网络下载下来的二进制数据显示
+    //UIImage *image2 = [UIImage imageWithData:<#(NSData *)#>];
+    
+    NSString *path2 = [[NSBundle mainBundle] pathForResource:@"q" ofType:@"jpg"];
+    NSData *data = [NSData dataWithContentsOfFile:path2];
+    
+    UIImage *image2 = [UIImage imageWithData:data scale:[UIScreen mainScreen].scale];
+    NSLog(@"image2:width=%f, height=%f", image2.size.width, image2.size.height);
+    
+    UIImageView *imageView2 = [[UIImageView alloc] initWithImage:image2];
+    imageView2.frame = CGRectMake(0, 300, image2.size.width, image2.size.height);
+    [self.view addSubview:imageView2];
+
+    //3.第三种方式
+    //传入图片的名称
+    //能够存储缓存,适用于使用多次的图片和较小的图片
+    //能够自动的适配图片
+    //player10.png  player10@2x.png  player10@3x.png
+//    UIImage *image3 = [UIImage imageNamed:@"player10.png"];
+
 }
 
 @end
