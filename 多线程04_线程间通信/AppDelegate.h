c2fc939786e71d390bf8e30895cf7d41d6cd18eb
Yuen-rMBP <rxg9527@sina.cn>
2016-03-29 23:41:53 +0800

    0329 多线程03_线程间通信 初始化

---
+++ 多线程04_线程间通信/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  多线程04_线程间通信
+//
+//  Created by Yuen on 16/3/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
