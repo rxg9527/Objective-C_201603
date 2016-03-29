e419eaa256c0baec3fcb73e761e405927ebe741d
Yuen-rMBP <rxg9527@sina.cn>
2016-03-29 11:55:22 +0800

    0329 简单使用NSThread

--- 多线程02_NSThread/ViewController.m
+++ 多线程02_NSThread/ViewController.m
@@ -20,7 +20,17 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    
+    // 创建线程
+    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(run:) object:@"Yuen"];
+    // 启动线程
+    [thread start];
+}
+
+- (void)run:(NSString *)parameter {
+    /*输出结果：run---Yuen---<NSThread: 0x7ff540406760>{number = 1, name = (null)}---<NSThread: 0x7ff5421334f0>{number = 3, name = (null)}*/
+    for (NSUInteger i = 0; i < 10000; i++) {
+        NSLog(@"run---%@---%@---%@", parameter, [NSThread mainThread], [NSThread currentThread]);
+    }
 }
 
 @end
