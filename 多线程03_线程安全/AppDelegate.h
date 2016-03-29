94aeb2e7704ab2cff899c8231ac7974d02b4f618
Yuen-rMBP <rxg9527@sina.cn>
2016-03-29 23:23:33 +0800

    0329 多线程03_线程安全 初始化

---
+++ 多线程03_线程安全/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  多线程03_线程安全
+//
+//  Created by Yuen on 16/3/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 多线程03_线程安全/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  多线程03_线程安全
+//
+//  Created by Yuen on 16/3/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
