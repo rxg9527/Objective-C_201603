2a292e0a216de45287868416e4e7dd510a3e5f52
Yuen-rMBP <rxg9527@sina.cn>
2016-03-29 11:48:33 +0800

    0329 NSThread初始化

---
+++ 多线程02_NSThread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  多线程02_NSThread
+//
+//  Created by Yuen on 16/3/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 多线程02_NSThread/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  多线程02_NSThread
+//
+//  Created by Yuen on 16/3/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+}
+
+@end
