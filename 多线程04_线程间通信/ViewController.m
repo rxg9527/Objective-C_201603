a89ed26585b93ecacc18dceba35323586454edd5
Yuen-rMBP <rxg9527@sina.cn>
2016-03-29 23:57:50 +0800

    0329 使用另一种方法获取时间：CFAbsoluteTime begin = CFAbsoluteTimeGetCurrent();

--- 多线程04_线程间通信/ViewController.m
+++ 多线程04_线程间通信/ViewController.m
@@ -22,9 +22,28 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self download];
+    [self download2];
 }
 
+- (void)download2 {
+    // 图片的网络路径
+    NSURL *url = [NSURL URLWithString:@"http://map.onegreen.net/%E4%B8%AD%E5%9B%BD%E6%94%BF%E5%8C%BA2500.jpg"];
+    
+    CFAbsoluteTime begin = CFAbsoluteTimeGetCurrent();
+    NSLog(@"%f", begin);
+//    NSDate *beginDate = [NSDate date];
+    // 根据图片的网络路径去下载图片数据
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    CFAbsoluteTime end = CFAbsoluteTimeGetCurrent();
+//    NSDate *endDate = [NSDate date];
+//    NSLog(@"%f", [endDate timeIntervalSinceDate:beginDate]);
+    NSLog(@"%f %f", end, end - begin);
+    
+    // 显示图片
+    self.imageView.image = [UIImage imageWithData:data];
+}
+
+/*主线程下载图片，耗时11.622523*/
 - (void)download {
     // 图片的网络路径
     NSURL *url = [NSURL URLWithString:@"http://map.onegreen.net/%E4%B8%AD%E5%9B%BD%E6%94%BF%E5%8C%BA2500.jpg"];
