5e132b7103a24901230332a4179d2f8b5dd4f4f0
Yuen-rMBP <rxg9527@sina.cn>
2016-03-29 12:05:29 +0800

    0329 给线程命名，用-isMainThread判断线程是否是主线程，用+isMainThread判断方法是否在主线程中运行

--- 多线程02_NSThread/ViewController.m
+++ 多线程02_NSThread/ViewController.m
@@ -21,24 +21,35 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self createThread1];
+}
+
+- (void)createThread2 {
+    
+}
+
+- (void)createThread1 {
     /*
      1、 简单使用NSThread
-    // 创建线程
-    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(run:) object:@"Yuen"];
-    // 启动线程
-    [thread start];
+     // 创建线程
+     NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(run:) object:@"Yuen"];
+     // 启动线程
+     [thread start];
      */
     
     /*2、 验证XGThread生命周期*/
     XGThread *thread = [[XGThread alloc] initWithTarget:self selector:@selector(run:) object:@"XG"];
-    [thread start]; 
+    thread.name = @"XG-Thread";
+    NSLog(@"%d", [thread isMainThread]);
+    [thread start];
 }
 
 - (void)run:(NSString *)parameter {
     /*输出结果：run---Yuen---<NSThread: 0x7ff540406760>{number = 1, name = (null)}---<NSThread: 0x7ff5421334f0>{number = 3, name = (null)}*/
     for (NSUInteger i = 0; i < 10000; i++) {
-        NSLog(@"run---%@---%@---%@", parameter, [NSThread mainThread], [NSThread currentThread]);
+        NSLog(@"run---%@---%@---%@", parameter, [NSThread mainThread], [NSThread currentThread].name);
     }
+    NSLog(@"%d", [NSThread isMainThread]);
 }
 
 @end
