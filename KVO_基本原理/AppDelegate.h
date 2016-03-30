e699ee3a69cf159ed1ba87f2ce26a8870d7e24e8
Yuen-rMBP <rxg9527@sina.cn>
2016-03-30 23:49:48 +0800

    0330 KVO_基本原理 初始化

---
+++ KVO_基本原理/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVO_基本原理
+//
+//  Created by Yuen on 16/3/30.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVO_基本原理/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVO_基本原理
+//
+//  Created by Yuen on 16/3/30.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
