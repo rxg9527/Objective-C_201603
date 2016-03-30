25c3d0d334b4e6856bce3c416cd55a595ff62137
Yuen-rMBP <rxg9527@sina.cn>
2016-03-30 21:19:49 +0800

    0330 GCD基本使用 初始化

---
+++ GCD01_基本使用/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCD01_基本使用
+//
+//  Created by Yuen on 16/3/30.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
